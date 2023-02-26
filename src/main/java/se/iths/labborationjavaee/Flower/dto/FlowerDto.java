5196a4a3e9da18b6ba83fd02156e753f1507e944
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-02-26 14:52:40 +0100

    add FlowerDto.java with code

--- src/main/java/se/iths/labborationjavaee/Flower/dto/FlowerDto.java
+++ src/main/java/se/iths/labborationjavaee/Flower/dto/FlowerDto.java
@@ -1,4 +1,37 @@
 package se.iths.labborationjavaee.Flower.dto;
 
+import se.iths.labborationjavaee.Flower.entity.Flower;
+
 public class FlowerDto {
+    private Long id;
+    private String name;
+
+    public FlowerDto() {
+    }
+
+    public FlowerDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public FlowerDto(Flower flower) {
+        this.id = flower.getId();
+        this.name = flower.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
