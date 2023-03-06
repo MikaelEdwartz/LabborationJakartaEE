d7103d3a0325594356fc8bca4900233978896bc8
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-06 11:43:28 +0100

    Renamed to Controller

--- src/main/java/se/iths/labborationjavaee/Flower/controller/Controller.java
+++ src/main/java/se/iths/labborationjavaee/Flower/controller/Controller.java
@@ -10,21 +10,20 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import se.iths.labborationjavaee.Flower.dto.FlowerDto;
-import se.iths.labborationjavaee.Flower.mapper.FlowerMapper;
-import se.iths.labborationjavaee.Flower.repository.FlowerRepository;
+import se.iths.labborationjavaee.Flower.mapper.Mapper;
+import se.iths.labborationjavaee.Flower.repository.Repository;
 import se.iths.labborationjavaee.Flower.resources.Attributes;
 
 import java.net.URI;
 import java.util.List;
 
 @Path("/flowers")
-public class FoodController {
+public class Controller {
 
     @Inject
-    FlowerRepository repository;
-
+    Repository repository;
     @Inject
-    FlowerMapper mapper;
+    Mapper mapper;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -81,23 +80,29 @@ public class FoodController {
             @ApiResponse(responseCode = "200", description = "Flower updated",
                     content = @Content(schema = @Schema(implementation = FlowerDto.class))),
             @ApiResponse(responseCode = "404", description = "Id not found"),
-            @ApiResponse(responseCode = "400", description = "No property to update")}
-    )
+            @ApiResponse(responseCode = "400", description = "No property to update")})
     public Response updateFlower(@PathParam("id") Long id, @QueryParam("name") String name, @QueryParam("color") String color) {
+        checkIfValidInput(id, name, color);
+        callRepositoryAndChangeValue(id, name, color);
+
+        return Response.ok().entity(mapper.map(repository.findById(id).get())).build();
+    }
+
+    private void checkIfValidInput(Long id, String name, String color) {
         if (repository.findById(id).isEmpty())
             throw new NotFoundException("id:" + id);
 
         if (isAttributesNull(name, color))
             throw new IllegalArgumentException("At least one parameter is required.");
+    }
 
+    private void callRepositoryAndChangeValue(Long id, String name, String color) {
         if (color == null)
             repository.changeAttributes(id, name, null, Attributes.NAME);
         else if (name == null)
             repository.changeAttributes(id, null, color, Attributes.COLOR);
         else
             repository.changeAttributes(id, name, color, Attributes.BOTH);
-
-        return Response.ok().entity(mapper.map(repository.findById(id).get())).build();
     }
 
     private static boolean isAttributesNull(String name, String color) {
